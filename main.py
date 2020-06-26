5890672f28e9cc81caa61d8b9e4681fb9ca908bc
Ligrade <ligrade13@gmail.com>
2020-06-26 10:29:09 +0200

    ping7.2

--- main.py
+++ main.py
@@ -178,12 +178,8 @@ async def pingg(ctx):
 
 @bot.command(pass_context=True)
 async def ping(ctx):
-    """ Pong! """
-    await delete_message(ctx.message)
-    before = time.monotonic()
-    message = await ctx.send("Pong!")
-    ping = (time.monotonic() - before) * 1000
-    await message.edit(content=f"Pong!  `{int(ping)}ms`")
-    print(f'Ping {int(ping)}ms')
+    now = datetime.datetime.utcnow()
+    delta = now - ctx.message.timestamp
+    await bot.say('{}ms'.format(delta(microseconds=1)))
 
 bot.run(bot.run(os.environ['TOKEN']))

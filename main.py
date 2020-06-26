827ae20190f2729c81ade0a23bfa309eb5f7d315
Ligrade <ligrade13@gmail.com>
2020-06-26 11:13:32 +0200

    8ball1.2

--- main.py
+++ main.py
@@ -182,24 +182,13 @@ async def _8ball(ctx, *, question):
             "Oui.",
             "Non.",
             "Sans aucun doute.",
-            "Yes - definitely.",
-            "You may rely on it.",
-            "As I see it, yes.",
-            "Most likely.",
-            "Outlook good.",
-            "Yes.",
-            "Signs point to yes.",
-            "Reply hazy, try again.",
-            "Ask again later.",
-            "Better not tell you now.",
-            "Cannot predict now.",
-            "Concentrate and ask again.",
-            "Don't count on it.",
-            "My reply is no.",
-            "My sources say no.",
-            "Outlook not so good.",
-            "Very doubtful."]
-  await ctx.send(f'Question: {question}\nAnswer: {random.choice(responses)}')
+            "Biensûr.",
+            "Je ne pense pas."]
+  await ctx.send(f'**Question :** {question}\n**Réponce :** {random.choice(responses)}')
+
+@bot.command(aliases=['8ball'])
+async def _8ball(ctx):
+    await ctx.send(f"Entrez **.8ball <Votre question>** pour continuer")
 
 
 bot.run(bot.run(os.environ['TOKEN']))

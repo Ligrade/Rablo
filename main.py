8eb21d0fdc974ac29435aa75a56949ade5c8a1db
Ligrade <ligrade13@gmail.com>
2020-06-26 10:55:48 +0200

    roll1.3

--- main.py
+++ main.py
@@ -177,8 +177,19 @@ async def ping(ctx):
     await ctx.send(f'🏓 Pong! `{round(bot.latency * 1000)}`ms')
 
 @bot.command()
-async def roll(ctx):
-    cards = ['1','2','3','4']
-    await self.send_message(channel, "You chose number {}!".format(random.choice(cards)))
+async def roll(ctx, sides, amount):
+  try:
+    sides = int(sides.split("d")[1])
+    rolls_list = []
+    for number in range(int(amount)):
+       # 1 is the minimum number the dice can have
+       rolls_list.append(random.randint(1, sides))
+    rolls = ", ".join(str(number) for number in rolls_list)
+    await ctx.send("Your dice rolls were: " + rolls)
+  except Exception as e:
+    # You should catch different exceptions for each problem and then handle them
+    # Exception is too broad
+    print(e)
+    await ctx.send("Incorrect format for sides of dice (try something like \"!roll d6 1\").")
 
 bot.run(bot.run(os.environ['TOKEN']))

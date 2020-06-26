640c6e009492830f0b88b4fbe8b08cc836987022
Ligrade <ligrade13@gmail.com>
2020-06-26 16:04:01 +0200

    msondage1.4.2

--- main.py
+++ main.py
@@ -203,21 +203,8 @@ async def msondage(ctx):
 	except:
 		await ctx.send("Veuillez réitérer la commande.")
 		return
-	message = await ctx.send(f"**Sondage de {author.name}:** {recette.content}")
+	message = await ctx.send(f"**Sondage:** {recette.content}")
 	await message.add_reaction("✅")
 	await message.add_reaction("❌")
-
-
-	def checkEmoji(reaction, user):
-		return ctx.message.author == user and message.id == reaction.message.id and (str(reaction.emoji) == "✅" or str(reaction.emoji) == "❌")
-
-	try:
-		reaction, user = await bot.wait_for("reaction_add", timeout = 60, check = checkEmoji)
-		if reaction.emoji == "✅":
-			await ctx.send("La recette a démarré.")
-		else:
-			await ctx.send("La recette a bien été annulé.")
-	except:
-		await ctx.send("La recette a bien été annulé.")
 
 bot.run(bot.run(os.environ['TOKEN']))

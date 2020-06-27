8cf6b3ee0e0a9657a80cc0815bcefc97d13b7820
Ligrade <ligrade13@gmail.com>
2020-06-27 17:42:48 +0200

    roulette1.2

--- main.py
+++ main.py
@@ -227,29 +227,29 @@ async def chinese(ctx, *text):
 
 @bot.command()
 async def roulette(ctx):
-	await ctx.send("La roulette commencera dans 10 secondes. Envoyez \"moi\" dans ce channel pour y participer.")
+	await ctx.send("La roulette commencera dans 10 secondes. Envoyez **\"join\"** dans ce channel pour y participer.")
 	
 	players = []
 	def check(message):
-		return message.channel == ctx.message.channel and message.author not in players and message.content == "moi"
+		return message.channel == ctx.message.channel and message.author not in players and message.content == "join" "Join"
 
 	try:
 		while True:
-			participation = await bot.wait_for('message', timeout = 10, check = check)
+			participation = await bot.wait_for('message', timeout = 15, check = check)
 			players.append(participation.author)
 			print("Nouveau participant : ")
 			print(participation)
-			await ctx.send(f"**{participation.author.name}** participe au tirage ! Le tirage commence dans 10 secondes")
+			await ctx.send(f"**{participation.author.name}**, participe au tirage ! Le tirage commence dans 15 secondes")
 	except: #Timeout
 		print("Demarrage du tirrage")
 
 	gagner = ["ban", "kick", "role personnel", "mute", "gage"]
 
-	await ctx.send("Le tirage va commencer dans 3...")
+	await ctx.send("Le tirage va commencer dans **3**...")
 	await asyncio.sleep(1)
-	await ctx.send("2")
+	await ctx.send("**2**")
 	await asyncio.sleep(1)
-	await ctx.send("1")
+	await ctx.send("**1**")
 	await asyncio.sleep(1)
 	loser = random.choice(players)
 	price = random.choice(gagner)

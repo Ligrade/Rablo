f1d79182efab983ff506599d297a6686f442d2b3
Ligrade <ligrade13@gmail.com>
2020-06-27 19:17:46 +0200

    ytbdl

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ import random
 import aiohttp
 import logging
 import asyncio
+import youtube_dl
 import discord
 from discord.ext import commands
 import datetime
@@ -252,5 +253,7 @@ async def roulette(ctx):
 	await asyncio.sleep(1)
 	loser = random.choice(players)
 	await ctx.send(":boom::gun: **POoUM**!!! `" + loser.name + "`" + " est mort!")
+
+#Music
 
 bot.run(bot.run(os.environ['TOKEN']))

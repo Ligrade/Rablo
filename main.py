2586b3e02791632bdc7ebb543950d3ac2a807b28
Ligrade <ligrade13@gmail.com>
2020-06-28 04:31:37 +0200

    fin1

--- main.py
+++ main.py
@@ -260,85 +260,72 @@ async def roulette(ctx):
 
 #Music commandes
 
-# musics = {}
-# ytdl = youtube_dl.YoutubeDL()
+musics = {}
+ytdl = youtube_dl.YoutubeDL()
 
-# class Video:
-#     def __init__(self, link):
-#         video = ytdl.extract_info(link, download=False)
-#         video_format = video["formats"][0]
-#         self.url = video["webpage_url"]
-#         self.stream_url = video_format["url"]
+class Video:
+    def __init__(self, link):
+        video = ytdl.extract_info(link, download=False)
+        video_format = video["formats"][0]
+        self.url = video["webpage_url"]
+        self.stream_url = video_format["url"]
 
-# @bot.command(aliases=['j', 'joi'])
-# async def join(ctx):
+@bot.command()
+async def leave(ctx):
+    client = ctx.guild.voice_client
+    await client.disconnect()
+    musics[ctx.guild] = []
 
-# @bot.command()
-# async def leave(ctx):
-#     client = ctx.guild.voice_client
-#     await client.disconnect()
-#     musics[ctx.guild] = []
-
-# @bot.command()
-# async def resume(ctx):
-#     client = ctx.guild.voice_client
-#     if client.is_paused():
-#         client.resume()
+@bot.command()
+async def resume(ctx):
+    client = ctx.guild.voice_client
+    if client.is_paused():
+        client.resume()
 
 
-# @bot.command()
-# async def pause(ctx):
-#     client = ctx.guild.voice_client
-#     if not client.is_paused():
-#         client.pause()
+@bot.command()
+async def pause(ctx):
+    client = ctx.guild.voice_client
+    if not client.is_paused():
+        client.pause()
 
 
-# @bot.command()
-# async def skip(ctx):
-#     client = ctx.guild.voice_client
-#     client.stop()
+@bot.command()
+async def skip(ctx):
+    client = ctx.guild.voice_client
+    client.stop()
 
 
-# def play_song(client, queue, song):
-#     source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(song.stream_url
-#         , before_options = "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5"))
+def play_song(client, queue, song):
+    source = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(song.stream_url
+        , before_options = "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5"))
 
-#     def next(_):
-#         if len(queue) > 0:
-#             new_song = queue[0]
-#             del queue[0]
-#             play_song(client, queue, new_song)
-#         else:
-#             asyncio.run_coroutine_threadsafe(client.disconnect(), bot.loop)
+    def next(_):
+        if len(queue) > 0:
+            new_song = queue[0]
+            del queue[0]
+            play_song(client, queue, new_song)
+        else:
+            asyncio.run_coroutine_threadsafe(client.disconnect(), bot.loop)
 
-#     client.play(source, after=next)
+    client.play(source, after=next)
 
 
-# @bot.command()
-# async def play(ctx, url):
-#     print("play")
-#     client = ctx.guild.voice_client
+@bot.command()
+async def play(ctx, url):
+    print("play")
+    client = ctx.guild.voice_client
 
-#     if client and client.channel:
-#         video = Video(url)
-#         musics[ctx.guild].append(video)
-#     else:
-#         channel = ctx.author.voice.channel
-#         video = Video(url)
-#         musics[ctx.guild] = []
-#         client = await channel.connect()
-#         await ctx.send(f"Je lance : {video.url}")
-#         play_song(client, musics[ctx.guild], video)
+    if client and client.channel:
+        video = Video(url)
+        musics[ctx.guild].append(video)
+    else:
+        channel = ctx.author.voice.channel
+        video = Video(url)
+        musics[ctx.guild] = []
+        client = await channel.connect()
+        await ctx.send(f"Je lance : {video.url}")
+        play_song(client, musics[ctx.guild], video)
  
-#musique ++
-
-@client.command(pass_context=True)
-async def join(ctx):
-    channel = ctx.message.author.voice.voice_channel
-    await client.join_voice_channel(channel)
-    count = 0
-    for m in music_list.getMusics():
-        musics[count] = m
-        count += 1
 
 bot.run(bot.run(os.environ['TOKEN']))
